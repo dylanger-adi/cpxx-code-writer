9c4046da2d7b39b7b1db956602a836908d321d85
dylanger-adi <113053239+dylanger-adi@users.noreply.github.com>
2022-09-07 11:06:30 -0700

    V0.3
    Added initial LCD support

--- EN_WRITEBOX.ino
+++ EN_WRITEBOX.ino
@@ -1,15 +1,26 @@
-//Dylanger Gutierrez, 2022-06-21
-//Version 0.2
-//Added functions to read buttons and drive output pin.
+//Dylanger Gutierrez, 2022-06-23
+//Version 0.3
+//Added initial LCD support.
 //
 //This is the code to drive the box to control the interface for CP parts.  Normally high, where 1 and 0 are determined by negative width.
+
+//junk for the LCD Screen
+#include "Arduino.h"
+#if defined(ARDUINO_ARCH_SAMD) || defined(__SAM3X8E__)
+  // use pin 18 with Due, pin 1 with Zero or M0 Pro 
+  #define lcd Serial1 
+#else
+  #include <SoftwareSerial.h>
+  // Create a software serial port!
+  SoftwareSerial lcd = SoftwareSerial(0,2);
+#endif
 
 #include <stdio.h>
 //define necessary pins
 #define INVPIN 2
 
-#define W0PIN 3
-#define W1PIN 4
+#define W0PIN 4
+#define W1PIN 3
 #define CLRPIN 5
 #define WRTPIN 6
 
@@ -73,6 +84,14 @@ void loop() {
     
   }
   updateScreen(buttonState,writebuf,buflength);
+  if(buttonState==4) {
+    for(int i=0;i<3;i++) {
+      digitalWrite(LEDPIN,HIGH);
+      delay(100);
+      digitalWrite(LEDPIN,LOW);
+      delay(30);
+    }
+  }
 }
 
 int readButtons() {       //checks which button pressed
@@ -108,7 +127,7 @@ void writeBits(bool invState,unsigned int wbuf,int blen) {  //writes bits to out
     for(int k=blen-1;k>=0;k--) {
       if(wbuf & (1<<k)) {
         digitalWrite(ENPIN, LOW);
-        delayMicroseconds(tENHI);
+        //delayMicroseconds(tENHI);
         digitalWrite(ENPIN, HIGH);
         delayMicroseconds(TEN-tENHI);
       } else {
@@ -136,6 +155,37 @@ void writeBits(bool invState,unsigned int wbuf,int blen) {  //writes bits to out
   return;
 }
 
-void updateScreen(int buttonState,unsigned int writebuf,int buflength) {
+void updateScreen(int buttonState,unsigned int writebuf,int blen) {
+  char buftext[17];
+  buftext[16]='\0'; //set null character
+  for(int k=0; k<(16-blen);k++) { //fill leader with spaces
+    buftext[15-k]=' ';
+  }
+  for(int k=0;k<blen;k++) { //fill leader with spaces
+    if((writebuf>>(blen-1-k)) & 1) { //puts a '1' for 1 bits and a '0' for 0
+      buftext[k]='1';
+    } else {
+      buftext[k]='0';
+    }
+  }
+  for(int k=blen+1;k>=blen;k--) { //initialize both lines of text to NULL
+    buftext[k]=' ';
+  }
+  lcd.write(0xFE); // clear screen
+  lcd.write(0x58);
+  delay(10);
+  lcd.write(0xFE); // go 'home' to origin
+  lcd.write(0x48);
+  delay(10);
+  if(buttonState==1 | buttonState==2) { //W0 or W1 case
+    lcd.println("Bits to write:  ");
+    lcd.println(buftext);
+  } else if(buttonState==3) {
+    lcd.println("Buffer Cleared. ");    //CLR case
+    lcd.println("");
+  } else if(buttonState==4) {
+    lcd.println("Written Bits:   ");    //WRT case
+    lcd.println(buftext);
+  }    
   return;
 }
