6ebcba10299592d66c40cca5d0959ece05e6226e
dylanger-adi <113053239+dylanger-adi@users.noreply.github.com>
2022-09-07 10:59:17 -0700

    EN_WRITEBOX.ino V0.2

--- EN_WRITEBOX.ino
+++ EN_WRITEBOX.ino
@@ -1,3 +1,7 @@
+//Dylanger Gutierrez, 2022-06-21
+//Version 0.2
+//Added functions to read buttons and drive output pin.
+//
 //This is the code to drive the box to control the interface for CP parts.  Normally high, where 1 and 0 are determined by negative width.
 
 #include <stdio.h>
@@ -13,48 +17,125 @@
 
 #define ENPIN 9
 
+#define LEDPIN 11
+
 #define tENHI 1     //in us
 #define tENLO 100   //in us
+#define TEN 500     //in us. Period must be longer than tENLO
 
 bool invertOut=false;
+bool invSwitchState=false;
 int buflength=0;
 unsigned int writebuf=0; //should be 16 bit
+int buttonState=0;
 
 void setup() {
-  // put your setup code here, to run once:
-  
+  pinMode(INVPIN, INPUT); //Switch to control if the box is normally high or low
+  pinMode(W0PIN, INPUT);  //button; adds a '0' to the buffer
+  pinMode(W1PIN, INPUT);  //button; adds a '1' to the buffer
+  pinMode(CLRPIN, INPUT); //button; clears write buffer
+  pinMode(WRTPIN, INPUT); //button; writes buffer to the output
 
+  pinMode(ENPIN, OUTPUT); //Drives with digital output signal
+  pinMode(LEDPIN, OUTPUT);//Blinks when buffer written
+  digitalWrite(ENPIN,LOW);
+  digitalWrite(LEDPIN,LOW);
+  
 }
 
 void loop() {
-//invertswitchstate = read(norm Hi Switch)
-//if(invertOut & !invertswitchstate)
-//  invertswitchstate=true
-//  write output high
-//else if (!invertOut & invertswitchstate)
-//  invertswitchstate=false
-//  write output low
-//buttonState=readButtons() //Button State: 0= no read; 1= "W0"; 2= "W1"; 3="CLR"; 4="WRT"
-//
-//if (W0 and buflength<16):
-//  Shift buffer to the left one bit
-//  length++
-//else if (W1 and buflength<16):
-//  Shift buffer to the left one bit and add 1
-//  length++
-//else if CLR:
-//  writebuf=0
-//  buflength=0
-//else if WRITE:
-//  writeBits(invertState,writebuf,buflength)
-//  writebuf=0
-//  buflength=0
-//if buttonState>0:
-//  updateScreen(buttonState,writebuf,buflength)
-//
-//
+  invSwitchState = digitalRead(INVPIN);
+  if(invertOut & (!invSwitchState)) { //non-inverting case if switch is off
+    invertOut=false;
+    digitalWrite(ENPIN,LOW);
+  }
+  if((!invertOut) & invSwitchState) { //inverting case if switch is on; normal mode of operation
+    invertOut=true;
+    digitalWrite(ENPIN,HIGH);
+  }
+  buttonState = readButtons(); //Button State: 0= no read; 1= "W0"; 2= "W1"; 3="CLR"; 4="WRT"
+  if((buttonState==1) & buflength<16) {
+    writebuf = writebuf<<1; //shifts left and leaves lsb=0
+    buflength++;    
+  }
+  if((buttonState==2) & buflength<16) {
+    writebuf = (writebuf<<1) + 1; //shifts left and leaves lsb=0
+    buflength++;
+  }
+  if(buttonState==3) {
+    writebuf = 0;
+    buflength = 0;
+  }
+  if(buttonState==4) {
+    writeBits(invertOut,writebuf,buflength);
+  }
+  if(buttonState>0) {
+    
+  }
+  updateScreen(buttonState,writebuf,buflength);
 }
 
-int readButtons() {
-  
+int readButtons() {       //checks which button pressed
+  if(digitalRead(W0PIN)) {
+    delay(50);
+    while(digitalRead(W0PIN));
+    delay(50);
+    return 1;
+  }
+  if(digitalRead(W1PIN)) {
+    delay(50);
+    while(digitalRead(W1PIN));
+    delay(50);
+    return 2;
+  }
+  if(digitalRead(CLRPIN)) {
+    delay(50);
+    while(digitalRead(CLRPIN));
+    delay(50);
+    return 3;
+  }
+  if(digitalRead(WRTPIN)) {
+    delay(50);
+    while(digitalRead(WRTPIN));
+    delay(50);
+    return 4;
+  }
+  return 0;
+}
+
+void writeBits(bool invState,unsigned int wbuf,int blen) {  //writes bits to output
+  if(invState) {
+    for(int k=blen-1;k>=0;k--) {
+      if(wbuf & (1<<k)) {
+        digitalWrite(ENPIN, LOW);
+        delayMicroseconds(tENHI);
+        digitalWrite(ENPIN, HIGH);
+        delayMicroseconds(TEN-tENHI);
+      } else {
+        digitalWrite(ENPIN, LOW);
+        delayMicroseconds(tENLO);
+        digitalWrite(ENPIN, HIGH);
+        delayMicroseconds(TEN-tENLO);
+      }
+    }
+  } else {
+    for(int k=blen-1;k>=0;k--) {
+      if(wbuf & (1<<k)) {
+        digitalWrite(ENPIN, HIGH);
+        delayMicroseconds(tENHI);
+        digitalWrite(ENPIN, LOW);
+        delayMicroseconds(TEN-tENHI);
+      } else {
+        digitalWrite(ENPIN, HIGH);
+        delayMicroseconds(tENLO);
+        digitalWrite(ENPIN, LOW);
+        delayMicroseconds(TEN-tENLO);
+      }
+    }
+  }
+  return;
+}
+
+void updateScreen(int buttonState,unsigned int writebuf,int buflength) {
+  return;
 }
