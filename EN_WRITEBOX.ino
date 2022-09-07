df1361d1531078bc60df30d9cb163d3ca6b575f6
dylanger-adi <113053239+dylanger-adi@users.noreply.github.com>
2022-09-07 11:11:53 -0700

    V0.4
    Further implementation of buttons and LCD work.

--- EN_WRITEBOX.ino
+++ EN_WRITEBOX.ino
@@ -1,6 +1,6 @@
-//Dylanger Gutierrez, 2022-06-23
-//Version 0.3
-//Added initial LCD support.
+//Dylanger Gutierrez, 2022-08-09
+//Version 0.4
+//Added LCD and implementation work.
 //
 //This is the code to drive the box to control the interface for CP parts.  Normally high, where 1 and 0 are determined by negative width.
 
@@ -12,7 +12,7 @@
 #else
   #include <SoftwareSerial.h>
   // Create a software serial port!
-  SoftwareSerial lcd = SoftwareSerial(0,2);
+  SoftwareSerial lcd = SoftwareSerial(0,7); //pin 7 is LCD
 #endif
 
 #include <stdio.h>
@@ -51,7 +51,58 @@ void setup() {
   pinMode(LEDPIN, OUTPUT);//Blinks when buffer written
   digitalWrite(ENPIN,LOW);
   digitalWrite(LEDPIN,LOW);
+
+  //Below is LCD Stuff
+  lcd.begin(9600);  
   
+  // set the size of the display if it isn't 16x2 
+  lcd.write(0xFE);
+  lcd.write(0xD1);
+  lcd.write(16);  // 16 columns
+  lcd.write(2);   // 2 rows
+  delay(10);       
+
+  // set the contrast, 200 is a good place to start, adjust as desired
+  lcd.write(0xFE);
+  lcd.write(0x50);
+  lcd.write(200);
+  delay(10);       
+  
+  // set the brightness 
+  lcd.write(0xFE);
+  lcd.write(0x99);
+  lcd.write(255);
+  delay(10);       
+  
+  // turn off cursors
+  lcd.write(0xFE);
+  lcd.write(0x4B);
+  lcd.write(0xFE);
+  lcd.write(0x54);
+  delay(10);
+
+   // scroll behavior
+  lcd.write(0xFE);
+  lcd.write(0x52);
+  delay(10);
+
+  //writes splash screen
+  lcd.write(0xFE);
+  lcd.write(0x40);
+  lcd.write("Code Writer v0.4by Dylanger G.  ");//32 chars; version number
+  delay(10);
+
+  // clear screen
+  lcd.write(0xFE); 
+  lcd.write(0x58);
+  delay(10);
+
+  //set backlight neutral
+  writeBacklightColor(63,63,63);
+
+  //splash screen initial text
+  lcd.write("Code Writer v0.4by Dylanger G.  ");//32 chars; version number
+
 }
 
 void loop() {
@@ -81,9 +132,9 @@ void loop() {
     writeBits(invertOut,writebuf,buflength);
   }
   if(buttonState>0) {
-    
+    updateScreen(buttonState,writebuf,buflength);
   }
-  updateScreen(buttonState,writebuf,buflength);
+  
   if(buttonState==4) {
     for(int i=0;i<3;i++) {
       digitalWrite(LEDPIN,HIGH);
@@ -92,6 +143,7 @@ void loop() {
       delay(30);
     }
   }
+  buttonState=0;
 }
 
 int readButtons() {       //checks which button pressed
@@ -127,7 +179,7 @@ void writeBits(bool invState,unsigned int wbuf,int blen) {  //writes bits to out
     for(int k=blen-1;k>=0;k--) {
       if(wbuf & (1<<k)) {
         digitalWrite(ENPIN, LOW);
-        //delayMicroseconds(tENHI);
+        //delayMicroseconds(tENHI); too slow already
         digitalWrite(ENPIN, HIGH);
         delayMicroseconds(TEN-tENHI);
       } else {
@@ -141,7 +193,7 @@ void writeBits(bool invState,unsigned int wbuf,int blen) {  //writes bits to out
     for(int k=blen-1;k>=0;k--) {
       if(wbuf & (1<<k)) {
         digitalWrite(ENPIN, HIGH);
-        delayMicroseconds(tENHI);
+        //delayMicroseconds(tENHI);
         digitalWrite(ENPIN, LOW);
         delayMicroseconds(TEN-tENHI);
       } else {
@@ -168,8 +220,8 @@ void updateScreen(int buttonState,unsigned int writebuf,int blen) {
       buftext[k]='0';
     }
   }
-  for(int k=blen+1;k>=blen;k--) { //initialize both lines of text to NULL
-    buftext[k]=' ';
+  for(int k=17;k>=blen;k--) { //initialize both lines of text to NULL
+    buftext[k]='\0';
   }
   lcd.write(0xFE); // clear screen
   lcd.write(0x58);
@@ -177,15 +229,52 @@ void updateScreen(int buttonState,unsigned int writebuf,int blen) {
   lcd.write(0xFE); // go 'home' to origin
   lcd.write(0x48);
   delay(10);
-  if(buttonState==1 | buttonState==2) { //W0 or W1 case
-    lcd.println("Bits to write:  ");
-    lcd.println(buftext);
-  } else if(buttonState==3) {
-    lcd.println("Buffer Cleared. ");    //CLR case
-    lcd.println("");
+  if(buttonState==1 ) { //W0 or W1 case
+    lcd.print("Bits to write:  ");
+    lcd.write(0xFE); // move write position
+    lcd.write(0x47);
+    lcd.write(16-blen+1);//position in row
+    lcd.write(2);//second column
+    delay(10);
+    lcd.print(buftext);
+    delay(10);
+    writeBacklightColor(63,63,255); //Blue
+  } else if(buttonState==2) {
+    lcd.print("Bits to write:  ");
+    lcd.write(0xFE); // move write position
+    lcd.write(0x47);
+    lcd.write(16-blen+1);//position in row
+    lcd.write(2);//second column
+    delay(10);
+    lcd.print(buftext);
+    delay(10);
+    writeBacklightColor(255,100,0); //yellow
+  } else if((buttonState==3)&(blen>0)) {
+    lcd.print("Buffer Cleared. ");
+    delay(10);
+    writeBacklightColor(255,20,20); //red
   } else if(buttonState==4) {
-    lcd.println("Written Bits:   ");    //WRT case
-    lcd.println(buftext);
+    lcd.print("Written Bits:   ");
+    lcd.write(0xFE); // move write position
+    lcd.write(0x47);
+    lcd.write(16-blen+1);//position in row
+    lcd.write(2);//second column
+    delay(10);
+    lcd.print(buftext);
+    delay(10);
+    writeBacklightColor(20,255,20); //green
   }    
+  //lcd.print(buttonState,DEC); //diagnostic
+  delay(100);
   return;
 }
+
+void writeBacklightColor(byte R, byte G, byte B) {
+  lcd.write(0xFE);
+  lcd.write(0xD0);
+  lcd.write(R);
+  lcd.write(G);
+  lcd.write(B);
+  return;
+}
+ 
